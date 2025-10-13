0dc02944212a540432340b984576cd83f6415dc4
Abhijeet4002 <141422358+Abhijeet4002@users.noreply.github.com>
2025-10-13 17:41:29 +0530

    Add production origin to CORS options

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://jobportal-v5ft.onrender.com'],
     credentials: true
 }
 
